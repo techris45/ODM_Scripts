ea412978259e1b25b9a5636fd5366d18a3078e54
Saijin-Naib <19295950+Saijin-Naib@users.noreply.github.com>
2020-03-17 16:19:54 -0400

    Create webodm_upgrade_ubuntu.sh
    Attempt to modify webodm_upgrade.sh so that it will work properly under Ubuntu 20.04 Focal Fossa.

---
+++ webodm_upgrade_ubuntu.sh
@@ -0,0 +1,28 @@
+source venv/bin/activate
+#djangorestframework djangorestframework-filters django-filter django-guardian djangorestframework-guardian tzlocal numpy drf-nested-routers
+
+cd ../../..
+
+cd /code
+git pull origin master
+sudo bash configure.sh reinstall
+
+cd /www
+git checkout package.json
+git pull origin master
+npm install
+
+cd /webodm
+git checkout package.json
+git pull origin master
+
+
+npm install
+python3 -m pip install -r requirements.txt
+webpack --mode production
+python3 manage.py collectstatic --noinput
+python3 manage.py migrate
+bash app/scripts/plugin_cleanup.sh
+echo "from app.plugins import build_plugins;build_plugins()" | python3 manage.py shell
+
+sudo chown -R odm:odm /code /www /webodm /home/odm
